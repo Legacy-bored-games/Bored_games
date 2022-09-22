9a9dfe295a214327fca70f7968045d9421a7dd0f
Margarita Marmaridou <margmar@protonmail.com>
2022-09-22 17:38:53 +0300

    Home and About components done

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,15 +1,24 @@
+//Styled Components
+import { StyledAbout } from "../styles/About.styled";
+//ReactRouter
+import { NavLink } from "react-router-dom";
+
 function About() {
   return (
-    <div>
+    <StyledAbout>
       <h1>About Bored Games</h1>
       <p>
         Bored Games is a web app that aspires to connect people over their love
         for board games. Find people near you who wish to play the same board
         games as you. Host board game sessions, attend board game sessions
         hosted by others, make new friends, expand your knowledge of board games
-        and, above all, HAVE FUN! Sign up now.
+        and, above all, HAVE FUN!&nbsp;
+        <NavLink to="/signup" style={{ color: "tomato" }}>
+          Sign up now
+        </NavLink>
+        .
       </p>
-    </div>
+    </StyledAbout>
   );
 }
 
