30f29da77f41aadb28e7a29dbf6197aa23a055d1
Margarita Marmaridou <margmar@protonmail.com>
2022-09-23 14:22:57 +0300

    login form & signup form

---
+++ frontend/src/components/styles/SignupForm.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledFormTitle = styled.h1`
+    font-family:'Poppins';
+    font-size: 2.5em;
+    margin-bottom: 0;
+    margin-top: -1.5em;
+
+`
+
+export const StyledButton = styled.button`
+    width: 100%;
+    padding: .2em;
+    border: none;
+    border-radius: 2em;
+    align-self: center;
+    background-color: tomato;
+    font-family: 'Poppins';
+    font-size: 1.5em;
+    font-weight: 500;
+    color: white;
+    margin-bottom: .5em;
+    
+
+    &:hover {
+        cursor: pointer;
+        background-color: black;
+    }
+    
+
+`
