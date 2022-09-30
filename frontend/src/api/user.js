1e95a2e8e9253b47715bc05e1edfb46c4f44d82e
Ksenia Rachouti <blossomingiris@gmail.com>
2022-09-30 11:33:42 +0300

    fix api request

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,10 +1,9 @@
-import{post,get,patch,remove} from "./request"
+import { post, get, patch, remove } from './request'
 
-export const login=data=>post('/user/login',data) ;
-export const signUp =data=>post('/user/signup',data) ;
+export const login = (data) => post('/user/login', data)
+export const signUp = (data) => post('/user/signup', data)
 
-
-export const getUsers=()=>get('/user') ;
-export const getUser=id=>get(`/user/${id}`) ;
-export const updateUser=(id,data)=>patch(`/user/${id}`,data) ;
-export const deleteUser=(id)=>remove(`/user/${id}`) ;
+export const getUsers = () => get('/user')
+export const getUser = (id) => get(`/user/${id}`)
+export const updateUser = (id, data) => patch(`/user/${id}`, data)
+export const deleteUser = (id) => remove(`/user/${id}`)
